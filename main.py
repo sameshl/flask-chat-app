e2efdf2955af89d03011b909442455189bae7847
Samesh Lakhotia <samesh.lakhotia@gmail.com>
2020-06-06 11:25:00 +0530

    Remove parts not needed for private chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit
-from flask_sqlalchemy import SQLAlchemy
 
 import time
 
@@ -8,28 +7,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'some_secret_value'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
-db = SQLAlchemy(app)
-
 # make a global users dict
 users = {}
-
-class History(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)
-    message = db.Column('message', db.String(500))
-
-
-
-@socketio.on('message')
-def handle_message(msg):
-    print('Session ID ' + request.sid)
-    print('Message ' + msg)
-    # Add message to the db
-    message = History(message=msg)
-    db.session.add(message)
-    db.session.commit()
-    # send the message to clients
-    send(msg, broadcast=True)
 
 @socketio.on('username', namespace='/private')
 def receive_username(username):
@@ -52,8 +31,7 @@ def private_message(payload):
 
 @app.route('/')
 def index():
-    messages = History.query.all()
-    return render_template('index.html', messages=messages)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
